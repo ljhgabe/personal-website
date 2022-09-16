1e06a6e08e6bfe2f0a96f78a2997907ad07e0253
Geyu Lin <geyu.lin@agoda.com>
2022-09-16 16:51:34 +0800

    add detailed experience

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./stars.scss";
 import Typed from "react-typed";
+import Image from 'react-image-resizer'
+import nus from "../img/nus_logo.jpeg"
+import paypal from "../img/paypal_logo.png"
+import agoda from "../img/agoda_logo.jpeg"
 
 class Experience extends React.Component {
     constructor() {
@@ -9,18 +13,29 @@ class Experience extends React.Component {
             experience: [
                 {id: 1,
                 company: "PayPal", 
+                logo: paypal,
                 title: "Software Engineer Intern", 
-                Responsibility: "", 
+                Responsibility: ["Develop Internal Issue Tracker using ReactJS, Springboot and Mongo DB", 
+                                "Design API for website to fetch issue information from JIRA",
+                                "Support Auto email of the summary and analysis of issue",
+                                "Improve regression testing by creating java aspect to precreate data and fetch data directly during test time",
+                                "Reduce regression test time 40%"], 
                 Date:"2021 Jan - 2021 July"},
                 {id: 2,
-                company: "NUS Ubicomp Lab", 
+                company: "NUS Ubicomp Lab",
+                logo: nus, 
                 title: "Research Intern", 
-                Responsibility: "", 
+                Responsibility: ["Develop Android Application to support data collection from Esense earbuds device (Accelerometer, gyroscope)",
+                                "Collect face touching activity data using earbuds sensor for 30 participants", 
+                                "Implement 1DCNN, LSTM model for activity recognition (classification)"], 
                 Date:"2020 Dec - 2022 May"},
                 {id: 3,
                 company: "Agoda", 
+                logo: agoda, 
                 title: "Data Science and Machine Learning Intern", 
-                Responsibility: "", 
+                Responsibility: ["Deisgn the system to support online reinforcement learning",
+                                "Implement general Reinforcement Learning framework with different exploration algorithm", 
+                                "Adapt different use cases with respect to the framework"], 
                 Date:"2022 Aug - Present"},
             ]
         }
@@ -44,11 +59,21 @@ class Experience extends React.Component {
                                 return (
                                     <React.Fragment>
                                     <div className="col-sm-12">
-                                    <div className="box-shadow-full">
+                                    <div className="box-shadow-full" >
+            
+                                    <center><Image src={experience.logo} height={100} width={100} style={{ alignSelf: 'center' }}/></center>
                                     <div><b>Company:</b> {experience.company} </div>
                                     <div><b>Title:</b> {experience.title} </div>
                                     <div><b>Responsibility:</b></div>
-                                    <div>{experience.Responsibility}</div>
+                                        {
+                                            experience.Responsibility.map(
+                                                item => {
+                                                    return (
+                                                        <li key="{item}">{item}</li>
+                                                    );
+                                                }
+                                            )
+                                        }
                                     <div><b>Duration: </b>{experience.Date}</div>
                                     </div>
                                     </div>
